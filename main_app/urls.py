a69906bd447059b168e7061d296188e7d47bfcad
xcbhx <ceina.ellison@gmail.com>
2022-11-15 22:00:07 -0500

    implement user stories

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/',views.about, name='about'),
-    # route for dogs index
     path('dogs/', views.dogs_index, name='index'),
+    path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -15,3 +15,9 @@ def dogs_index(request):
     return render(request, 'dogs/index.html', {
         'dogs': dogs
     })
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', {
+        'dog': dog
+    })
