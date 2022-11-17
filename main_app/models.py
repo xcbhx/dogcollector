710b42bd5c6be9ff0b051f811edc16cd2255d665
xcbhx <ceina.ellison@gmail.com>
2022-11-17 15:04:28 -0500

    feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+)
+
+
 # Create your models here.
 class Dog(models.Model):
   name = models.CharField(max_length=100)
@@ -9,12 +16,27 @@ class Dog(models.Model):
   age = models.IntegerField()
 
 
-
-# Changing this instance method
-# does not impact the database, therefore
-# no makemigrations is necessary
   def __str__(self):
     return f'{self.name} ({self.id})'
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'dog_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  dog = models.ForeignKey(
+    Dog, 
+    on_delete=models.CASCADE
+    )
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
